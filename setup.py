aae03261ccb5cadc8a602bd96e65db53278c2cd6
Nikhil Singh <nik.singh710@gmail.com>
2021-07-12 20:47:49 +0530

    Silly fix --version modified
    Fixed the pip install command as pypi replaces '_' with '-'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='hello-niksingh710',
-    version='0.0.1',
+    version='0.0.2',
     description='Says Hello!',
     py_modules=['hello_niksingh710'],
     package_dir={'': 'src'},
